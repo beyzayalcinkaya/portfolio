8e2435172801bc2a33ceea19500af1c3c815cc17
beyzayalcinkaya <ylcnkybeyza666@gmail.com>
2025-08-09 18:51:06 +0300

    Update resume link and remove underline

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Hero.css";
 import profileImage from "../../assets/homePageImage.jpeg";
+import resumePdf from "../../assets/resume.pdf";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Hero = () => {
   return (
@@ -11,12 +13,28 @@ const Hero = () => {
         <span>I'm Beyza Yalçınkaya,</span> software developer based in Turkey.
       </h1>
       <p>
-        I'am a software developer in Istanbul. I'am a electrical and electronics
+        I'm a software developer in Istanbul. I'am a electrical and electronics
         engineer.
       </p>
       <div className="heroAction">
-        <div className="heroConnect">Connect with me</div>
-        <div className="heroResume">My Resume </div>
+        <div className="heroConnect">
+          {" "}
+          <AnchorLink className="anchorLink" offset={50} href="#contact">
+            Connect With Me{" "}
+          </AnchorLink>
+        </div>
+
+        <a
+          href={resumePdf}
+          download
+          style={{
+            textDecoration: "none",
+            color: "inherit",
+            cursor: "pointer",
+          }}
+        >
+          <div className="heroResume">My Resume</div>
+        </a>
       </div>
     </div>
   );
