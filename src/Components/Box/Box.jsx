a2a90fef4134e91e3028e0dcbba6db0067cde8a7
beyzayalcinkaya <ylcnkybeyza666@gmail.com>
2025-09-24 22:08:03 +0300

    reading time

--- src/Components/Box/Box.jsx
+++ src/Components/Box/Box.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Box.scss";
 
-const Box = ({ id, title, desc1 }) => {
+const Box = ({ id, title, desc1, readingTime }) => {
   return (
     <Link to={`/blog/${id}`} className="box">
       <h2 className="text">{title}</h2>
       <h6 className="text-desc">{desc1}</h6>
+      <p className="reading-time">Okuma süresi: {readingTime} dk</p>
     </Link>
   );
 };
